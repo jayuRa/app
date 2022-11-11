d594ae3994e3681eb531c3aba35656617040b3b7
jayuRa <mayank.ramani@varianceinfotech.com>
2022-11-11 16:16:10 +0530

    demo

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app=FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"hello":"word"}
+
+
+@app.get("/items/{item_id}")    
+def read_item(item_id:int):
+    return {"item_id":item_id}
+
